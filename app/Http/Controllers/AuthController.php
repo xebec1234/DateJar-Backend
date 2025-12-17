bb5ab0605344a1f0376a71005ec2f383f713d783
admin <manueljosepera487@gmail.com>
2025-12-17 17:02:20 +0800

    added login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,9 +1,11 @@
 <?php
 
+
 namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -23,5 +25,22 @@ class AuthController extends Controller
          $token = $user->createToken('API Token')->plainTextToken;
 
     return response()->json(['user' => $user, 'token' => $token]);
+    }
+    
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
     }
 }
